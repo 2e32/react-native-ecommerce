d369e86c9fe17df211f755ace451178e116d8d43
konstantin b <bogucharov.dev@gmail.com>
2022-12-16 16:22:22 +0300

    Add bottom navigation

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import * as Page from './src/pages';
 
-const Stack = createNativeStackNavigator();
+import { colors } from './src/constants';
+
+import navbarIcons from './src/assets/icons/navbar/index.js';
+
+const Tab = createBottomTabNavigator();
+
+const screenOptions = ({ route }) => ({
+  headerStyle: {
+    backgroundColor: colors.white,
+  },
+  headerTintColor: colors.main,
+  headerTitleStyle: {
+    fontSize: 16,
+    fontWeight: '600',
+    lineHeight: 24,
+  },
+  tabBarIcon: ({ focused }) => {
+    let icon;
+
+    if (route.name === 'Home') {
+      icon = focused ? navbarIcons.homeFill : navbarIcons.home;
+    } else if (route.name === 'Favorite') {
+      icon = focused ? navbarIcons.markerFill : navbarIcons.marker;
+    } else if (route.name === 'Notification') {
+      icon = focused ? navbarIcons.bellFill : navbarIcons.bell;
+    } else if (route.name === 'Profile') {
+      icon = focused ? navbarIcons.personFill : navbarIcons.person;
+    }
+
+    return (
+      <Image
+        source={icon}
+        style={{
+          width: 24,
+          height: 24,
+          resizeMode: 'contain',
+        }}
+      />
+    );
+  },
+  tabBarShowLabel: false,
+  tabBarStyle: { height: 75 },
+});
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Page.Home} options={{ title: 'Welcome' }} />
-        <Stack.Screen name="Favorite" component={Page.Favorite} options={{ title: 'Fav' }} />
-        <Stack.Screen name="Profile" component={Page.Profile} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+        <Tab.Screen name="Home" component={Page.Home} />
+        <Tab.Screen name="Favorite" component={Page.Favorite} />
+        <Tab.Screen name="Notification" component={Page.Notification} />
+        <Tab.Screen name="Profile" component={Page.Profile} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
