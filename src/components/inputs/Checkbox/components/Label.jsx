3f3d8ca9f834e70e0b74365af9b815f213011c1e
2e32 <118853094+2e32@users.noreply.github.com>
2023-01-10 11:15:09 +0300

    Rename CheckBox to Checkbox (#11)

---
+++ src/components/inputs/Checkbox/components/Label.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text } from 'react-native';
+
+import { colors } from '../../../../constants';
+
+export default function Label(props) {
+  const { children, disabled = false, style, ...rest } = props;
+
+  return (
+    <Text style={[styles.label, disabled && styles.labelDisabled, style]} {...rest}>
+      {children}
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  label: {
+    marginLeft: 8,
+    color: colors.main,
+    fontSize: 18,
+    fontWeight: '400',
+    lineHeight: 24,
+  },
+  labelDisabled: {
+    color: colors.sub,
+  },
+});
