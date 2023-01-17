41f788b5ec673ba61f43ed7e299d47037e9dbf79
konstantin b <bogucharov.dev@gmail.com>
2023-01-17 23:10:13 +0300

    Update images links

--- src/constants/mocks.js
+++ src/constants/mocks.js
@@ -82,7 +82,7 @@ const chairs = [
     description:
       'Bar stools in the BERGMUND series offer cushiony comfort and many cover options, like this one in an elegant gray. After engaging meals and after-dinner chit-chat, it’s good to know it’s washable.',
     image:
-      'https://www.ikea.com/us/en/images/products/bergmund-bar-stool-with-backrest-black-gunnared-medium-gray__0926400_pe789189_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/bergmund-bar-stool-with-backrest-black-gunnared-medium-gray__0926401_pe789192_s5.jpg',
     price: 130,
     raiting: 4.9,
     reviews: 15,
@@ -94,7 +94,7 @@ const chairs = [
     description:
       'This comfy bar stool/counter stool is the perfect height for lazy breakfasts, casual meals or snacks at the counter or kitchen island. Made of easy-care materials so occasional spills are easy to wipe off.',
     image:
-      'https://www.ikea.com/us/en/images/products/stig-bar-stool-with-backrest-black-black__0948110_pe798867_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/stig-bar-stool-with-backrest-black-black__0948117_pe798878_s5.jpg',
     price: 24.99,
     raiting: 4,
     reviews: 41,
@@ -106,7 +106,7 @@ const chairs = [
     description:
       'Why not have the same style throughout your home? INGOLF series looks just as nice in the kitchen and dining room as in the bedroom or hallway. The solid wood construction holds up well.',
     image:
-      'https://www.ikea.com/us/en/images/products/ingolf-bar-stool-with-backrest-white__0728062_pe736035_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/ingolf-bar-stool-with-backrest-brown-black__0873567_pe600684_s5.jpg',
     price: 95,
     raiting: 4.2,
     reviews: 243,
@@ -118,7 +118,7 @@ const chairs = [
     description:
       "A comfortable bar stool with clean lines in white and chrome. If you have several you can stack them – handy when it's time to sweep the floor or if you just want some more floor space.",
     image:
-      'https://www.ikea.com/us/en/images/products/glenn-bar-stool-white-chrome-plated__0728083_pe736048_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/glenn-bar-stool-white-chrome-plated__0873987_pe653485_s5.jpg',
     price: 105,
     raiting: 4.6,
     reviews: 69,
@@ -130,7 +130,7 @@ const chairs = [
     description:
       'Big, bigger, biggest - our super comfy EKEDALEN chairs keep the conversation going until you ask the guests to leave. Available in 3 heights for a perfect match with EKEDALEN bar table or dining table.',
     image:
-      'https://www.ikea.com/us/en/images/products/ekedalen-bar-stool-with-backrest-dark-brown-orrsta-light-gray__0657893_pe710024_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/ekedalen-bar-stool-with-backrest-dark-brown-orrsta-light-gray__0873904_pe720397_s5.jpg',
     price: 90,
     raiting: 4.4,
     reviews: 34,
@@ -142,7 +142,7 @@ const chairs = [
     description:
       'NORDVIKEN counter-height stool with a deepening in the seat and curved backrest will keep you comfortably seated for long conversations at the kitchen island. The look is easy to combine in any kitchen.',
     image:
-      'https://www.ikea.com/us/en/images/products/nordviken-bar-stool-with-backrest-white__0714174_pe729957_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/nordviken-bar-stool-with-backrest-black__1053256_pe846850_s5.jpg',
     price: 95,
     raiting: 4.4,
     reviews: 34,
@@ -154,7 +154,7 @@ const chairs = [
     description:
       'A soft, padded seat with restful flexibility – that’s how it feels when you sit down for a nice long meal in VOLFGANG bar stool or dining chair – both are just as comfortable.',
     image:
-      'https://www.ikea.com/us/en/images/products/nordviken-bar-stool-with-backrest-white__0714174_pe729957_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/volfgang-bar-stool-with-backrest-chrome-plated-gunnared-medium-gray__0873677_pe700217_s5.jpg',
     price: 140,
     raiting: 4.3,
     reviews: 7,
@@ -166,7 +166,7 @@ const chairs = [
     description:
       'Bar stools in the BERGMUND series offer cushiony comfort and many cover options, like this one in stylish black leather.',
     image:
-      'https://www.ikea.com/us/en/images/products/bergmund-bar-stool-with-backrest-black-glose-black__0928322_pe789788_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/bergmund-bar-stool-with-backrest-black-glose-black__0928323_pe789789_s5.jpg',
     price: 200,
     raiting: 4.7,
     reviews: 6,
@@ -177,7 +177,7 @@ const chairs = [
     summary: 'Bar stool, birch, 29 "',
     description: 'Easy to move thanks to the hole in the seat.',
     image:
-      'https://www.ikea.com/us/en/images/products/nilsolle-bar-stool-birch__45919_pe142507_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/nilsolle-bar-stool-birch__1053177_pe846771_s5.jpg',
     price: 80,
     raiting: 3.5,
     reviews: 6,
@@ -189,7 +189,7 @@ const chairs = [
     description:
       'Sit back and enjoy your dinner at the bar table. With an upholstered seat and back, this stool provides extra nice seating comfort.',
     image:
-      'https://www.ikea.com/us/en/images/products/hansolle-bar-stool-black-brown__0987269_pe817486_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/hansolle-bar-stool-black-brown__0987270_pe817487_s5.jpg',
     price: 170,
     raiting: 5,
     reviews: 5,
@@ -204,7 +204,7 @@ const armchairs = [
     description:
       'Bringing new life to an old favourite. We first introduced this chair in the 1950’s. Some 60 years later we brought it back into the range with the same craftsmanship, comfort and appearance. Enjoy!',
     image:
-      'https://www.ikea.com/gb/en/images/products/strandmon-wing-chair-nordvalla-dark-grey__0325432_pe517964_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/strandmon-wing-chair-nordvalla-dark-gray__1116445_pe872501_s5.jpg',
     price: 229,
     raiting: 4.7,
     reviews: 196,
@@ -216,7 +216,7 @@ const armchairs = [
     description:
       'EKENÄSET armchair adds a stylish retro vibe to the room that’s inspired by 1950s Scandinavian design. The classic look goes anywhere in the home – and gives you sturdy and robust seating for many years.',
     image:
-      'https://www.ikea.com/gb/en/images/products/ekenaeset-armchair-kilanda-light-beige__1109687_pe870153_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/ekenaeset-armchair-kilanda-light-beige__1110707_pe870568_s5.jpg',
     price: 199,
     raiting: 4.8,
     reviews: 24,
@@ -227,7 +227,7 @@ const armchairs = [
     summary: 'Armchair, oak veneer/Knisa light beige',
     description: 'Layer-glued bent oak gives comfortable resilience.',
     image:
-      'https://www.ikea.com/gb/en/images/products/poaeng-armchair-oak-veneer-knisa-light-beige__0571595_pe666981_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/poaeng-armchair-birch-veneer-knisa-light-beige__0837295_pe666935_s5.jpg',
     price: 80,
     raiting: 4.7,
     reviews: 23,
@@ -239,7 +239,7 @@ const armchairs = [
     description:
       'NOLMYRA easy chair re-imagines comfort with mesh fabric stretched over the frame, making the need for foam unnecessary. The result is an elegantly engineered lounge chair at a low price.',
     image:
-      'https://www.ikea.com/gb/en/images/products/nolmyra-easy-chair-birch-veneer-grey__0152020_pe310348_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/nolmyra-chair-birch-veneer-gray__0836782_pe600883_s5.jpg',
     price: 50,
     raiting: 3.4,
     reviews: 14,
@@ -251,7 +251,7 @@ const armchairs = [
     description:
       'EKENÄSET armchair adds a stylish retro vibe to the room that’s inspired by 1950s Scandinavian design. The classic look goes anywhere in the home – and gives you sturdy and robust seating for many years.',
     image:
-      'https://www.ikea.com/gb/en/images/products/ekenaeset-armchair-jonsbyn-black__1109680_pe870147_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/ekenaeset-armchair-jonsbyn-black__1179058_pe895830_s5.jpg',
     price: 250,
     raiting: null,
     reviews: null,
@@ -266,7 +266,7 @@ const tables = [
     description:
       'A stable construction in solid wood and hardwood veneer that makes each table unique, with varying grain patterns and natural color shifts that are part of the charm of wood.',
     image:
-      'https://www.ikea.com/us/en/images/products/roenninge-bar-table-birch__1044552_pe842223_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/roenninge-bar-table-birch__1051123_pe844996_s5.jpg',
     price: 279,
     raiting: 5,
     reviews: 1,
@@ -278,7 +278,7 @@ const tables = [
     description:
       'This table suits both homes and businesses demanding everyday strength and durability. Easy to place in a kitchen, dining room, café or meeting room. The clean design combines well with many styles.',
     image:
-      'https://www.ikea.com/us/en/images/products/tommaryd-table-white-stained-oak-veneer-anthracite__0898690_pe782584_s5.jpg',
+      'https://www.ikea.com/us/en/images/products/tommaryd-table-white__0941033_pe795300_s5.jpg',
     price: 279,
     raiting: 5,
     reviews: 10,
@@ -288,12 +288,12 @@ const tables = [
     name: 'LAGKAPTEN / ALEX',
     summary: 'Desk, white, 55 1/8x23 5/8 "',
     description:
-      'Limited space doesn’t mean you have to say no to studying or working from home. This desk takes up little floor space yet still has two drawer units where you can store papers and other items.',
+      'Limited space doesn’t mean you have to say no to studying or working from home. This desk takes up little floor space yet still has a drawer unit where you can store papers and other important items.',
     image:
-      'https://www.ikea.com/us/en/images/products/lagkapten-alex-desk-white__1022432_pe832720_s5.jpg?',
-    price: 259,
-    raiting: 4.6,
-    reviews: 95,
+      'https://www.ikea.com/us/en/images/products/lagkapten-alex-desk-white__1028399_pe835336_s5.jpg',
+    price: 174,
+    raiting: 4.4,
+    reviews: 8,
   },
 ];
 
@@ -305,7 +305,7 @@ const beds = [
     description:
       'KLEPPSTAD has a stylish and modern design with a metal bed frame and a cozy textile headboard. Easy to both buy and take home since it’s sold as two compact packages. So practical and ',
     image:
-      'https://www.ikea.com/us/en/images/products/kleppstad-bed-frame-white-vissle-beige__1035340_pe840527_s5.jpg?',
+      'https://www.ikea.com/us/en/images/products/kleppstad-bed-frame-white-vissle-beige__1035341_pe840528_s5.jpg',
     price: 169,
     raiting: 4.6,
     reviews: 36,
