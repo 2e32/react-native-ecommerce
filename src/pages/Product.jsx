bcc41b69cf90d6a7646737c8ced492e55947fb9e
konstantin b <bogucharov.dev@gmail.com>
2023-03-06 23:02:42 +0300

    Add indent

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native';
 
 import { Counter } from '../components';
@@ -33,29 +33,44 @@ export default function Product() {
   };
 
   return (
-    <>
+    <View style={styles.container}>
       <Text style={styles.header}>{product?.name}</Text>
-      <Text style={styles.price}>$ {product?.price}</Text>
-      <Counter value={count} min={0} onChange={setCount} />
 
-      <Text>
+      <View style={styles.panel}>
+        <Text style={styles.price}>$ {product?.price}</Text>
+        <Counter value={count} min={0} onChange={setCount} />
+      </View>
+
+      <View style={styles.panel2}>
         <Text style={styles.raiting}>{product?.raiting}</Text>
         <Text style={styles.reviews}>({product?.reviews} reviews)</Text>
-      </Text>
+      </View>
+
       <Text style={styles.description}>{product?.description}</Text>
       <Button title="Add to favorite" onPress={handleAddToFavorite} />
       <Button title="Add to cart" onPress={handleAddToCart} />
 
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="<" onPress={handleGoBack} />
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
   header: {
     fontSize: 24,
     fontWeight: '600',
+  },
+  panel: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  panel2: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   price: {
     fontSize: 30,
@@ -66,10 +81,12 @@ const styles = StyleSheet.create({
     fontWeight: '800',
   },
   reviews: {
+    marginLeft: 18,
     color: colors.sub,
     fontSize: 14,
   },
   description: {
+    marginTop: 8,
     color: colors.sub,
     fontSize: 16,
     lineHeight: 20,
